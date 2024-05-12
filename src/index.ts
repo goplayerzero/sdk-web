f292572665d52d87d57737c335640696043b303b
Animesh Koratana <koratana@playerzero.app>
2024-05-12 05:52:36 -0400

    Update Resilence of SDK Injection (#3)
    * Fixes to web sdk
    
    * added github thing
    
    * testing
    
    * remove debug

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { PlayerZeroSdk } from './sdk/playerzero.js';
+import {PlayerZeroSdk, PlayerZeroWindow} from './sdk/playerzero.js';
 
-const PlayerZero = new PlayerZeroSdk();
+const PlayerZero: PlayerZeroWindow = new PlayerZeroSdk();
 export default PlayerZero;
